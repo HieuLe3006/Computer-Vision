9fe243bebca41db999ee30e3e4c295926a05c501
Hieu Le <letrungl@sfu.ca>
2021-12-06 03:56:06 -0800

    update epi correspondence

--- 5/project5_package/matlab/testTempleCoords.m
+++ 5/project5_package/matlab/testTempleCoords.m
@@ -8,12 +8,15 @@ im1 = imread('../data/im1.png');
 im2 = imread('../data/im1.png');
 load('../data/someCorresp.mat');
 F = eightpoint(pts1, pts2, M);
+load('../data/intrinsics.mat');
+E = essentialMatrix(F, K1, K2);
 
 % % Test eightpoints
 % displayEpipolarF(im1, im2, F);
 
-% Test epipolar correspondences
+% % Test epipolar correspondences
 epipolarMatchGUI(im1, im2, F);
+
 
 % save extrinsic parameters for dense reconstruction
 % save('../data/extrinsics.mat', 'R1', 't1', 'R2', 't2');
